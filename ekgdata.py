76b31da45aad03eca1ee5ef6012868b6903c3131
Le-Samuels <ds3081@mci4me.at>
2024-06-12 10:03:09 +0200

    update requieremnts

--- ekgdata.py
+++ ekgdata.py
@@ -1,47 +1,75 @@
-import json
 import pandas as pd
 import plotly.express as px
-
-# %% Objekt-Welt
-
-# Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
+import plotly.graph_objects as go
+from scipy.signal import find_peaks
 
 class EKGdata:
-
-## Konstruktor der Klasse soll die Daten einlesen
-
     def __init__(self, ekg_dict):
-        #pass
         self.id = ekg_dict["id"]
         self.date = ekg_dict["date"]
         self.data = ekg_dict["result_link"]
-        self.df = pd.read_csv(self.data, sep='\t', header=None, names=['Messwerte in mV','Zeit in ms',])
+        self.df = pd.read_csv(self.data, sep='\t', header=None, names=['Messwerte in mV', 'Zeit in ms'])
 
-
-    def make_plot(self):
-
-        # Erstellte einen Line Plot, der ersten 2000 Werte mit der Zeit aus der x-Achse
-        self.fig = px.line(self.df.head(2000), x="Zeit in ms", y="Messwerte in mV")
-        #return self.fig 
+    @staticmethod
     def load_by_id(ekg_id, ekg_dict):
         for ekg_index in ekg_dict:
             if ekg_index['id'] == ekg_id:
-                ekg = EKGdata(
-                    ekg_dict
-                )
-                return ekg
+                return EKGdata(ekg_index)
         return None
 
+    def peak_finder(self):
+        df_subset = self.df.head(2000)
+        peaks, _ = find_peaks(df_subset["Messwerte in mV"], height=350)
+        return peaks
+
+    def make_plot(self):
+        df_subset = self.df.head(2000)
+        fig = px.line(df_subset, x="Zeit in ms", y="Messwerte in mV", title="EKG-Daten mit Peaks (Erste 2000 Datenpunkte)")
+
+        peaks = self.peak_finder()
+
+        # Peaks als rote Punkte hinzufügen
+        fig.add_trace(go.Scatter(
+            x=df_subset["Zeit in ms"].iloc[peaks],
+            y=df_subset["Messwerte in mV"].iloc[peaks],
+            mode='markers',
+            marker=dict(color='red', size=8),
+            name='Peaks'
+        ))
+        return fig
+
+    def estimate_hr(self):
+        df_subset = self.df.head(2000)
+        peaks = self.peak_finder()
+        peak_count = len(peaks)
+        time_max = df_subset["Zeit in ms"].max()
+        time_min = df_subset["Zeit in ms"].min()
+        time_duration = (time_max - time_min) / 60000  # Umrechnung in Minuten
+        hr = peak_count / time_duration
+        return hr
+
+    def show_head(self):
+        print(self.df.head(2000))
 
 
-if __name__ == "_main_":
-    print("This is a module with some functions to read the EKG data")
-    file = open("data/person_db.json")
-    person_data = json.load(file)
-    ekg_dict = person_data[0]["ekg_tests"][0]
-    print(ekg_dict)
-    ekg = EKGdata(ekg_dict)
-    print(ekg.df)
-    print(ekg.df.head())
-    print("The current id =",ekg_dict["id"])
-    ekg.fig = px.line(ekg.df.head(2000), x="Zeit in ms", y="Messwerte in mV").show()
+
+
+if __name__ == "__main__":
+    # JSON-Datei und EKG-Daten laden
+    with open("data/person_db.json", 'r') as file:
+        persons_data = json.load(file)
+    
+    ekg_dicts = persons_data[0]["ekg_tests"]
+    ekg = EKGdata.load_by_id(1, ekg_dicts)
+    
+    # Zeige den Head der ersten 2000 Daten an
+    ekg.show_head()
+
+    # Plot mit markierten Peaks erstellen und anzeigen (erste 2000 Datenpunkte)
+    fig = ekg.make_plot()
+    fig.show()
+    
+    peak_array = ekg.peak_finder()
+    print("Um die Rechenzeit zu verkürzen, wurden nur die ersten 2000 Datenpunkte betrachtet.")
+    print("Number of Peaks:", peak_array.size)
+    print("Estimated Heart Rate:", ekg.estimate_hr())
--- main.py
+++ main.py
@@ -1,63 +1,39 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-from make_plot import make_plot
-from make_plot import get_zone_times
-from make_plot import power_mean
-from make_plot import power_max
+import json
+from PIL import Image
 from def_persons import Person
 from ekgdata import EKGdata
-tab_Person_EKG,tab_test = st.tabs(["Person_EKG","test"])
+from make_plot import make_plot, get_zone_times, power_mean, power_max
+
+
+tab_Person_EKG, tab_test = st.tabs(["Person EKG", "test"])
 
 with tab_Person_EKG:
-
-
-    if "current_user" not in st.session_state:
-        st.session_state.current_user = "None"
-
-    st.header("Person")
-
-
-    st.session_state.current_user = st.selectbox(
-         "Wähle Versuchsperson",
-         options= Person.get_names(Person.get_person_data())
-         , key="sbVersuchsperson")
-
+    st.title('EKG Analyse App')
 
     person_data = Person.get_person_data()
-    # Paket zum anzeigen der Bilder
-    from PIL import Image
-    person_dict = Person.find_person_data_by_name(st.session_state.current_user)
-    # Laden eines Bilds
-    image = Image.open(person_dict["picture_path"])
-    # Anzeigen eines Bilds mit Caption
-    st.image(image, caption=st.session_state.current_user)
-    #Geburtsjahr der Person anzeigen lassen
-    current_year = Person.find_person_data_by_name(st.session_state.current_user)["date_of_birth"]
-    st.write("Geburtsjahr der Person" ":", current_year)
-    
-    #Wahl der Tests einer Person ermöglichen
-    st.write("EKG Tests")
-    if "current_test" not in st.session_state:
-        st.session_state.current_test = "None"
 
-    person_ekg_tests = Person.get_EKG_tests_by_name(st.session_state.current_user)  # Update this line to get EKG tests for the selected person
-
-    st.session_state.current_test = st.selectbox(
-        "Wähle Test aus",
-        options=[test["id"] for test in person_ekg_tests],  # Only show tests for the selected person
-        key="sbTest"
+    # Auswahl der Versuchsperson
+    selected_user = st.selectbox(
+        "Versuchsperson",
+        options=Person.get_names(person_data)
     )
 
-    if st.session_state.current_test != "None":
-        selected_test = next(test for test in person_ekg_tests if test["id"] == st.session_state.current_test)
-        ekg = EKGdata(selected_test)
-        ekg.make_plot()
-        st.plotly_chart(ekg.fig)
-        st.write("EKG ID: ", selected_test["id"])
-        st.write("Datum: ", selected_test["date"])
-        duration = selected_test["duration"]
-        st.write("Duration (in seconds): ", duration)
     
+    st.write("Currently selected user: " + selected_user)
+    person_dict = Person.find_person_data_by_name(selected_user)
+    image = Image.open(person_dict["picture_path"])
+    st.image(image, caption=selected_user)
 
+    # EKG-Daten laden
+    ekg_dict = person_dict["ekg_tests"][0]
+    ekg = EKGdata(ekg_dict)
+
+    fig = ekg.make_plot()
+    st.plotly_chart(fig)
+
+    hr = ekg.estimate_hr()
+    st.write(f" Herzfrequenz von {selected_user} beträgt ca {hr:.2f} BPM")
+
+    st.write("EKG ID: ", ekg_dict["id"])
 
